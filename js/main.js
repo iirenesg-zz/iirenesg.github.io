5ce1babaddd779e0adec125bab14a9a94d7e89e9
irsanchez <irsanchez@backcountry.com>
2021-10-05 19:04:41 -0600

    Fix menu animation

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 import gsap from 'gsap';
 
 let menuOpen = false;
+const timeline = gsap.timeline();
+const navNode = document.querySelector('nav');
 
 document.querySelector('#nav-icon2').addEventListener('click', (event) => {
   event.currentTarget.classList.toggle('open');
-  const timeline = gsap.timeline();
-  const stagger = window.innerWidth > 600 ? 0.2 : 0.3;
+  const stagger = '0.1';
+  const duration = '0.3';
+  const delay = window.innerWidth > 600 ? '<+=0.3' : '>';
 
   if (menuOpen) {
-    timeline.to('.menu-item', { duration: 0.5, y: '-100vh', stagger, ease: 'power1', reversed: true, onComplete: () => {
-      document.querySelector('nav').classList.remove('is-open');
-    } })
+    timeline.reverse().eventCallback('onComplete', () => {
+      if (!menuOpen) navNode.classList.remove('is-open');
+    });
+  } else if (timeline.reversed()) {
+    navNode.classList.add('is-open');
+    timeline.play();
   } else {
-    document.querySelector('nav').classList.add('is-open');
-    timeline.to('.menu-item', { duration: 0.5, y: 0, stagger, ease: 'power1' })
+    navNode.classList.add('is-open');
+    timeline
+      .to('.menu-item', { duration, y: 0, stagger, ease: 'power1' })
+      .to('.color-slide', { duration, y: '100vh', stagger, ease: 'power1' }, delay)
   }
 
   menuOpen = !menuOpen;
