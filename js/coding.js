654d21036bbdbe23deb28f174ab96b563e8c0a73
irsanchez <irsanchez@backcountry.com>
2021-10-09 11:17:12 -0600

    Creative coding page adjustments

--- js/coding.js
+++ js/coding.js
@@ -3,9 +3,15 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-setVideoTriggers('#life-trigger');
-setVideoTriggers('#int-trigger');
-setVideoTriggers('#hero-trigger', '+=200');
+document.querySelector('#hero-trigger').addEventListener('canplaythrough', () => {
+  document.querySelector('.wrapper').classList.remove('hidden');
+  gsap.to('.load-wrap', { opacity: 0, duration: 0.3 });
+  gsap.to('.wrapper', { opacity: 1, duration: 0.3 });
+
+  setVideoTriggers('#life-trigger');
+  setVideoTriggers('#int-trigger');
+  setVideoTriggers('#hero-trigger', window.innerWidth > 600 ? '+=200' : '+=100');
+});
 
 function setVideoTriggers(selector, offsetEnd = '') {
   const element = document.querySelector(selector);
